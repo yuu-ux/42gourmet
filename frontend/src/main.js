b3e504cd7e8fd547636146ad1fcefda74351f44a
airi-i1998 <airi.inoue@generosity.co.jp>
2025-04-19 16:35:54 +0900

    fix: main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi'
+  }
+})
+
+const app = createApp(App)
+const appInt = () => {
+  app.use(vuetify).mount('#app')
+}
+
+appInt()
