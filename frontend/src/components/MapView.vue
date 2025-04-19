b0ea79abf7c2cb3708e586c52ab615c35de208f9
Tsunami-S <jvlipnitsk@gmail.com>
2025-04-19 16:19:55 +0900

    add app_view files

---
+++ frontend/src/components/MapView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="map" style="height: 500px; width: 100%;"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const map = ref(null)
+const stores = ref([])
+
+const initMap = () => {
+  map.value = new google.maps.Map(document.getElementById("map"), {
+	center: { lat: 35.6907274197, lng: 139.6877882220 },
+    zoom: 16
+  })
+  fetchStores()
+}
+
+const fetchStores = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/api/stores')
+    const data = await response.json()
+    stores.value = data
+    addMarkers()
+  } catch (error) {
+    console.error('failed to get informations for stores', error)
+  }
+}
+
+const addMarkers = () => {
+  stores.value.forEach(store => {
+    new google.maps.Marker({
+      position: { lat: store.latitude, lng: store.longitude },
+      map: map.value,
+      title: store.name
+    })
+  })
+}
+
+const loadGoogleMapsAPI = () => {
+  const script = document.createElement('script')
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+  script.async = true
+  window.initMap = initMap
+  document.head.appendChild(script)
+}
+
+onMounted(() => {
+  loadGoogleMapsAPI()
+})
+</script>
