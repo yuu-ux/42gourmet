8328c11990b46c1c5225a0229ec6133eddc04d21
yuu-ux <yuu-ux@users.noreply.github.com>
2025-06-11 09:17:14 +0000

    Apply Prettier Change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
-const { defineConfig } = require('/opt/app/node_modules/vite')
+const { defineConfig } = require('/opt/app/node_modules/vite');
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
