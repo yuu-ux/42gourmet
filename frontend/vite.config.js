356c3e147fe2785ca0c579c30dac550b76f1f571
yuu-ux <yuugaehara@gmail.com>
2025-06-11 17:44:32 +0900

    :bug: コンテナの立ち上げに失敗する不具合を修正

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
+const { defineConfig } = require('/opt/app/node_modules/vite')
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
