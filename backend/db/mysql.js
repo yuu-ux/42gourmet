11dc6a3431639a54e2af12bd99e63470e763bdc8
yuu-ux <yuugaehara@gmail.com>
2025-06-17 19:45:20 +0900

    :heavy_plus_sign: prisma を追加

--- backend/db/mysql.js
+++ backend/db/mysql.js
@@ -1,6 +1,9 @@
 import { readFile } from 'node:fs/promises';
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import { PrismaClient } from '../generated/prisma/client.js';
+
+export const prisma = new PrismaClient();
 
 dotenv.config();
 
