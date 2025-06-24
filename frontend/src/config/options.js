b68004b611778489ddcb99185ffb383457f58cb5
Tsunami-S <Tsunami-S@users.noreply.github.com>
2025-06-24 12:50:36 +0000

    Apply Prettier Change

--- frontend/src/config/options.js
+++ frontend/src/config/options.js
@@ -1,25 +1,25 @@
 export const genreOptions = [
-  { title: "和食", value: 1 },
-  { title: "中華", value: 2 },
-  { title: "洋食", value: 3 },
-  { title: "アジアン", value: 4 },
-  { title: "カフェ", value: 5 },
-  { title: "居酒屋", value: 6 },
-  { title: "その他", value: 7 },
+    { title: '和食', value: 1 },
+    { title: '中華', value: 2 },
+    { title: '洋食', value: 3 },
+    { title: 'アジアン', value: 4 },
+    { title: 'カフェ', value: 5 },
+    { title: '居酒屋', value: 6 },
+    { title: 'その他', value: 7 },
 ];
 
 export const reasonOptions = [
-  { title: "コスパが良い", value: 1 },
-  { title: "提供が早い", value: 2 },
-  { title: "味が最高", value: 3 },
-  { title: "栄養満点", value: 4 },
+    { title: 'コスパが良い', value: 1 },
+    { title: '提供が早い', value: 2 },
+    { title: '味が最高', value: 3 },
+    { title: '栄養満点', value: 4 },
 ];
 
 export const priceOptions = [1, 2, 3, 4];
 
 export const priceLabels = {
-  1: "~500",
-  2: "500~999",
-  3: "1000~1500",
-  4: "1500~",
+    1: '~500',
+    2: '500~999',
+    3: '1000~1500',
+    4: '1500~',
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,8 @@ import vuetify from 'vite-plugin-vuetify';
 export default defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
     resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
 });
