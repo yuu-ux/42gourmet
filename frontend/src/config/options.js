8a863d9df73b0a3072bc3f960f3fd5561cf4ede5
Tsunami-S <Tsunami-S@users.noreply.github.com>
2025-07-05 14:21:15 +0000

    Apply Prettier Change

--- frontend/src/config/options.js
+++ frontend/src/config/options.js
@@ -1,32 +1,32 @@
 export function createMap(options) {
-  return options.reduce((acc, option) => {
-    acc[option.value] = option.title;
-    return acc;
-  }, {});
+    return options.reduce((acc, option) => {
+        acc[option.value] = option.title;
+        return acc;
+    }, {});
 }
 
 export const genreOptions = [
-  { title: '和食', value: 1 },
-  { title: '中華', value: 2 },
-  { title: '洋食', value: 3 },
-  { title: 'アジアン', value: 4 },
-  { title: 'カフェ', value: 5 },
-  { title: '居酒屋', value: 6 },
-  { title: 'その他', value: 7 },
+    { title: '和食', value: 1 },
+    { title: '中華', value: 2 },
+    { title: '洋食', value: 3 },
+    { title: 'アジアン', value: 4 },
+    { title: 'カフェ', value: 5 },
+    { title: '居酒屋', value: 6 },
+    { title: 'その他', value: 7 },
 ];
 
 export const reasonOptions = [
-  { title: 'コスパが良い', value: 1 },
-  { title: '提供が早い', value: 2 },
-  { title: '味が最高', value: 3 },
-  { title: '栄養満点', value: 4 },
+    { title: 'コスパが良い', value: 1 },
+    { title: '提供が早い', value: 2 },
+    { title: '味が最高', value: 3 },
+    { title: '栄養満点', value: 4 },
 ];
 
 export const priceOptions = [
-  { title: '~500', value: 1 },
-  { title: '~999', value: 2 },
-  { title: '~1500', value: 3 },
-  { title: '1501~', value: 4 },
+    { title: '~500', value: 1 },
+    { title: '~999', value: 2 },
+    { title: '~1500', value: 3 },
+    { title: '1501~', value: 4 },
 ];
 
 export const priceMap = createMap(priceOptions);
